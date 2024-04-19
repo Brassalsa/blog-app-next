e6e54920db9dc59c448c5cf815f4cc9eba37099e
Brassalsa <sauravchauhan2k@gmail.com>
2024-04-19 15:22:08 +0530

    changes in editor config

---
+++ src/lib/config/editor/index.ts
@@ -0,0 +1,15 @@
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import Underline from "@tiptap/extension-underline";
+import { Extensions } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { linkConfig, starterConfig, textAlignConfig } from "./extensionConfig";
+
+export const editorExtensions: Extensions = [
+  StarterKit.configure(starterConfig),
+  Underline.configure(),
+  TextAlign.configure(textAlignConfig),
+  Link.configure(linkConfig),
+  Image.configure(),
+];
