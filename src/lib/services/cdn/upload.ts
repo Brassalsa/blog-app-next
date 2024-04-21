82a876e0dcc712a5ac4bc8b49601a1ec21cce8dc
Brassalsa <sauravchauhan2k@gmail.com>
2024-04-21 16:01:46 +0530

    add upload function

--- src/lib/services/cdn/upload.ts
+++ src/lib/services/cdn/upload.ts
@@ -1,1 +1,33 @@
-export default async function upload() {}
+"use server";
+
+import { UploadApiErrorResponse, UploadApiResponse } from "cloudinary";
+import cdn from ".";
+
+export default async function uploadFile(
+  formData: FormData,
+  key: string = "file"
+) {
+  const file = formData.get(key) as File;
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = new Uint8Array(arrayBuffer);
+  const res = await new Promise<
+    UploadApiResponse | UploadApiErrorResponse | undefined
+  >((resolve, reject) => {
+    cdn.uploader
+      .upload_stream(
+        {
+          tags: ["blog-assets"],
+        },
+        function (err, result) {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(result);
+        }
+      )
+      .end(buffer);
+  });
+
+  return res?.url;
+}
