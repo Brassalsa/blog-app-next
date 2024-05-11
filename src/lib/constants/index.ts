b5112cca585cd7eb264beae156b60a8f47b8a596
Brassalsa <sauravchauhan2k@gmail.com>
2024-05-11 15:21:13 +0530

    add about page and its constants

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -6,3 +6,14 @@ export enum ALL_CATEGORIES {
 }
 
 export const POST_PER_PAGE = 6;
+
+export const MediaLinks = {
+  logo: "Logo",
+  author: "Saurav Chauhan",
+  authorImg: "/avatar.jpeg",
+  authorEmail: "saurav2kk@gmail.com",
+  facebook: "https://www.facebook.com/saurav.chauhan.940098/",
+  insta: "https://www.instagram.com/bruhsalsa/",
+  github: "https://github.com/Brassalsa",
+  twitter: "https://twitter.com/Sauravc64320635",
+};
