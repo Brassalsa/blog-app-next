9b5aa77d0cb95ad3f1173e8275e3d7842b299192
Brassalsa <sauravchauhan2k@gmail.com>
2024-05-13 16:12:51 +0530

    add forward ref

--- src/components/AuthorUI.tsx
+++ src/components/AuthorUI.tsx
@@ -7,33 +7,34 @@ import { cn } from "@/lib/utils";
 type Props = {
   author: AuthorType;
   className?: string;
-
   withText?: boolean;
 };
-function AuthorUI({ author, className, withText = true }: Props) {
-  return (
-    <div className={cn("flex gap-2", className)}>
-      {author.image ? (
-        <Image
-          src={author.image}
-          alt="author-image"
-          width={80}
-          height={80}
-          className="object-cover aspect-square size-12 rounded-[1000px]"
-        />
-      ) : (
-        <UserCircle className="size-10" />
-      )}
-      {withText && (
-        <div className="flex flex-col justify-center items-start gap-1 text-sm truncate">
-          <div className="font-semibold truncate">{author.name || "N/A"}</div>
-          <div className="text-xs text-muted-foreground truncate">
-            {author.email || "N/A"}
+const AuthorUI = React.forwardRef<HTMLDivElement, Props>(
+  ({ author, className, withText = true }: Props, ref) => {
+    return (
+      <div className={cn("flex gap-2", className)} ref={ref}>
+        {author.image ? (
+          <Image
+            src={author.image}
+            alt="author-image"
+            width={80}
+            height={80}
+            className="object-cover aspect-square size-12 rounded-[1000px]"
+          />
+        ) : (
+          <UserCircle className="size-10" />
+        )}
+        {withText && (
+          <div className="flex flex-col justify-center items-start gap-1 text-sm truncate">
+            <div className="font-semibold truncate">{author.name || "N/A"}</div>
+            <div className="text-xs text-muted-foreground truncate">
+              {author.email || "N/A"}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
-}
+        )}
+      </div>
+    );
+  }
+);
 
 export default AuthorUI;
