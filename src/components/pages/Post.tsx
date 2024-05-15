7e4412e8f2f92b6470c5db83a7688c57397bdd22
Brassalsa <sauravchauhan2k@gmail.com>
2024-05-15 17:41:39 +0530

    refactor Post.tsx

---
+++ src/components/pages/Post.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { CategList } from "../Categories";
+import { formatDate } from "@/lib/utils/helpers";
+import Image from "next/image";
+import AuthorUI from "../AuthorUI";
+import Comments from "../comments";
+
+function Post({
+  id,
+  title,
+  image,
+  about,
+  category,
+  description,
+  createdAt,
+  updatedAt,
+  author,
+  comments,
+}: BlogPostType) {
+  return (
+    <div key={id} className="flex flex-col">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mb-6">
+        <div className="relative  min-h-52 max-w-full aspect-video mx-auto">
+          <Image
+            src={image}
+            alt="post-image"
+            sizes="700px"
+            fill
+            className="object-contain rounded-lg"
+          />
+        </div>
+        <div className="space-y-2">
+          <h2 className="text text-3xl font-semibold">{title}</h2>
+          <div className="flex gap-4 items-center text-sm">
+            <CategList list={[category]} />
+            <p className="text-muted-foreground">{formatDate(createdAt)}</p>
+          </div>
+          <AuthorUI author={author} />
+          <div className="text-lg ">{about}</div>
+        </div>
+      </div>
+      <div
+        className="text-pretty"
+        dangerouslySetInnerHTML={{
+          __html: description,
+        }}
+      />
+      <Comments comments={comments} postId={id} />
+    </div>
+  );
+}
+
+export default Post;
