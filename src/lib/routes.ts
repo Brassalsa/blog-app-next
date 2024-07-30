8655b8b9d99b4b5113ee31793e04cf0f8acee732
Brassalsa <sauravchauhan2k@gmail.com>
2024-07-30 15:38:28 +0530

    reafctor

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,12 +1,17 @@
 export const links = {
   home: "/",
+
   blogs: "/blogs",
   blog: (id: string) => "/blog/" + id,
   addBlog: "/blog/add",
   editBlog: (id: string) => "/blog/edit/" + id,
-  about: "/about",
-  account: "/account",
-  accountId: (id: string) => links.account + "/" + id,
-  signIn: "/sign-in",
   blogCat: (cat: string) => links.blogs + "/category/" + cat,
+
+  about: "/about",
+
+  account: "/account",
+  accountSettings: "/account/settings",
+  accountId: (id: string) => links.account + "/" + id,
+
+  signIn: "/sign-in",
 };
