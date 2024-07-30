284525e6aaedd6eeb762a62abb36e775544c3b69
Brassalsa <sauravchauhan2k@gmail.com>
2024-07-30 17:35:28 +0530

    reafctor and fix bugs

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -16,7 +16,7 @@ function SignIn() {
   const router = useRouter();
 
   if (status === "authenticated") {
-    router.back();
+    router.push("/");
   }
 
   return (
@@ -24,20 +24,20 @@ function SignIn() {
       <h3 className="font-semibold text-2xl">Sign in/up with</h3>
       <SignIn.Button
         className="border-orange-600 flex justify-center items-center gap-2 group overflow-hidden"
-        handleClick={() => signIn("google")}
+        onClick={() => signIn("google")}
         SvgIcon={GoogleSVG}
         label="Google"
       />
 
       <SignIn.Button
         className="border-black dark:border-white flex justify-center items-center gap-2 group overflow-hidden"
-        handleClick={() => signIn("github")}
+        onClick={() => signIn("github")}
         SvgIcon={GitHubSVG}
         label="Github"
       />
       <SignIn.Button
         className="border-blue-500 "
-        handleClick={() => signIn("facebook")}
+        onClick={() => signIn("facebook")}
         SvgIcon={FaceBookSVG}
         label="Facebook"
       />
@@ -48,15 +48,15 @@ function SignIn() {
 export default SignIn;
 
 type Props = PropsWithClassName & {
-  handleClick: Cb;
+  onClick: Cb;
   SvgIcon: React.FC<SvgType>;
   label: string;
 };
 
-SignIn.Button = ({ className, SvgIcon, label, handleClick }: Props) => {
+SignIn.Button = ({ className, SvgIcon, label, onClick }: Props) => {
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick}
       className={cn(
         "m-2 p-2 w-36 rounded-md border-2 border-primary rounded-r-full rounded-l-full flex justify-center items-center gap-2 group overflow-hidden",
         className
